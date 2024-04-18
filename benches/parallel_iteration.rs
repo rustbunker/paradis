3ebc15be34d1c6879a431f990eb2a2bb3c4ac3a4
Andreas Longva <andreas.b.longva@gmail.com>
2024-04-18 09:55:20 +0200

    Add a couple more benchmarks for parallel iteration of index subset

--- benches/parallel_iteration.rs
+++ benches/parallel_iteration.rs
@@ -1,8 +1,8 @@
 use divan::Bencher;
 use paradis::rayon::create_par_iter;
-use paradis::unique::narrow_access_to_indices;
-use paradis_core::IntoParAccess;
-use rayon::iter::{IntoParallelRefMutIterator, ParallelIterator};
+use paradis::unique::{CheckedIndexList, narrow_access_to_indices};
+use paradis_core::{IntoParAccess, ParAccess};
+use rayon::iter::{IntoParallelRefIterator, IntoParallelRefMutIterator, ParallelIterator};
 use std::hint::black_box;
 
 fn main() {
@@ -40,7 +40,7 @@ fn slice_baseline_access(bencher: Bencher, n: usize) {
 #[divan::bench(
     args = [1_000_000, 10_000_000, 100_000_000]
 )]
-fn slice_indexed_access(bencher: Bencher, n: usize) {
+fn slice_redundantly_indexed_access(bencher: Bencher, n: usize) {
     let mut data = vec![5; n];
 
     bencher.bench_local(|| {
@@ -48,5 +48,40 @@ fn slice_indexed_access(bencher: Bencher, n: usize) {
         let indices = 0..n;
         let access = narrow_access_to_indices(access, &indices);
         create_par_iter(access).for_each(|x| *x *= 2);
+    });
+}
+
+#[divan::bench(
+args = [1_000_000, 10_000_000, 100_000_000]
+)]
+fn slice_subset_indexed_access(bencher: Bencher, n: usize) {
+    let mut data = vec![5; n];
+
+    let indices: Vec<_> = (0 .. n).step_by(2).collect();
+    let indices = CheckedIndexList::from_hashable_indices(indices).unwrap();
+
+    bencher.bench_local(|| {
+        let access = black_box(data.as_mut_slice()).into_par_access();
+        let access = narrow_access_to_indices(access, &indices);
+        create_par_iter(access).for_each(|x| *x *= 2);
+    });
+}
+
+/// Perform unsafe indexing through an access for iterating over a subset of indices.
+#[divan::bench(
+args = [1_000_000, 10_000_000, 100_000_000]
+)]
+fn slice_subset_unsafe_access(bencher: Bencher, n: usize) {
+    let mut data = vec![5; n];
+
+    let indices: Vec<_> = (0 .. n).step_by(2).collect();
+
+    bencher.bench_local(|| {
+        let access = black_box(data.as_mut_slice()).into_par_access();
+        let indices = black_box(&indices);
+
+        indices
+            .par_iter()
+            .for_each(|idx| unsafe { *access.get_unsync_unchecked(*idx) *= 2 });
     });
 }
