2bfd126d0bc751c4b6f1b2e01f937537fe39ed69
Andreas Longva <andreas.b.longva@gmail.com>
2024-04-18 11:24:46 +0200

    Add single thread bench, rewrite parallel benchmarks to use more black box

--- benches/parallel_iteration.rs
+++ benches/parallel_iteration.rs
@@ -9,79 +9,105 @@ fn main() {
     divan::main()
 }
 
+fn warmup<F: FnMut()>(mut f: F) {
+    for _ in 0 .. 10000 {
+        f();
+    }
+}
+
 /// Get some baseline numbers for basic Rayon parallel iteration
 #[divan::bench(
-    args = [1_000_000, 10_000_000, 100_000_000]
+    args = [1_000_000]
 )]
 fn slice_baseline_rayon(bencher: Bencher, n: usize) {
-    let mut data = vec![5; n];
+    let mut data = black_box(vec![5; n]);
+    let factor = black_box(2);
 
-    bencher.bench_local(|| {
-        black_box(&mut data).par_iter_mut().for_each(|x| *x *= 2);
-    });
+    let mut bench_fn = || {
+        black_box(&mut data).par_iter_mut().for_each(|x| *x *= factor);
+    };
+
+    warmup(&mut bench_fn);
+    bencher.bench_local(bench_fn);
 }
 
 /// Get baseline numbers for parallel iterator created through rayon
 #[divan::bench(
-    args = [1_000_000, 10_000_000, 100_000_000]
+    args = [1_000_000]
 )]
 fn slice_baseline_access(bencher: Bencher, n: usize) {
-    let mut data = vec![5; n];
+    let mut data = black_box(vec![5; n]);
+    let factor = black_box(2);
 
-    bencher.bench_local(|| {
+    let mut bench_fn = || {
         let access = black_box(data.as_mut_slice()).into_par_access();
-        create_par_iter(access).for_each(|x| *x *= 2);
-    });
+        create_par_iter(access).for_each(|x| *x *= factor);
+    };
+
+    warmup(&mut bench_fn);
+    bencher.bench_local(bench_fn);
 }
 
 /// Get baseline numbers for "redundantly indexed" access.
 /// Ideally this should compile down to basically the same as
 /// the standard Rayon parallel iterator.
 #[divan::bench(
-    args = [1_000_000, 10_000_000, 100_000_000]
+    args = [1_000_000]
 )]
 fn slice_redundantly_indexed_access(bencher: Bencher, n: usize) {
-    let mut data = vec![5; n];
+    let mut data = black_box(vec![5; n]);
+    let factor = black_box(2);
 
-    bencher.bench_local(|| {
+    let mut bench_fn = || {
         let access = black_box(data.as_mut_slice()).into_par_access();
         let indices = 0..n;
         let access = narrow_access_to_indices(access, &indices);
-        create_par_iter(access).for_each(|x| *x *= 2);
-    });
+        create_par_iter(access).for_each(|x| *x *= factor);
+    };
+
+    warmup(&mut bench_fn);
+    bencher.bench_local(bench_fn);
 }
 
 #[divan::bench(
-args = [1_000_000, 10_000_000, 100_000_000]
+args = [1_000_000]
 )]
 fn slice_subset_indexed_access(bencher: Bencher, n: usize) {
-    let mut data = vec![5; n];
+    let mut data = black_box(vec![5; n]);
+    let factor = black_box(2);
 
     let indices: Vec<_> = (0 .. n).step_by(2).collect();
     let indices = CheckedIndexList::from_hashable_indices(indices).unwrap();
 
-    bencher.bench_local(|| {
+    let mut bench_fn = || {
         let access = black_box(data.as_mut_slice()).into_par_access();
         let access = narrow_access_to_indices(access, &indices);
-        create_par_iter(access).for_each(|x| *x *= 2);
-    });
+        create_par_iter(access).for_each(|x| *x *= factor);
+    };
+
+    warmup(&mut bench_fn);
+    bencher.bench_local(bench_fn);
 }
 
 /// Perform unsafe indexing through an access for iterating over a subset of indices.
 #[divan::bench(
-args = [1_000_000, 10_000_000, 100_000_000]
+args = [1_000_000]
 )]
 fn slice_subset_unsafe_access(bencher: Bencher, n: usize) {
-    let mut data = vec![5; n];
+    let mut data = black_box(vec![5; n]);
+    let factor = black_box(2);
 
     let indices: Vec<_> = (0 .. n).step_by(2).collect();
 
-    bencher.bench_local(|| {
+    let mut bench_fn = || {
         let access = black_box(data.as_mut_slice()).into_par_access();
         let indices = black_box(&indices);
 
         indices
             .par_iter()
-            .for_each(|idx| unsafe { *access.get_unsync_unchecked(*idx) *= 2 });
-    });
+            .for_each(|idx| unsafe { *access.get_unsync_unchecked(*idx) *= factor });
+    };
+
+    warmup(&mut bench_fn);
+    bencher.bench_local(bench_fn);
 }
---
+++ benches/single_thread_iter.rs
@@ -0,0 +1,54 @@
+use divan::Bencher;
+use std::hint::black_box;
+use paradis::iter::create_iter;
+use paradis::unique::narrow_access_to_indices;
+use paradis_core::IntoParAccess;
+
+fn main() {
+    divan::main()
+}
+
+#[divan::bench(
+    args = [1_000_000]
+)]
+fn slice_baseline_std(bencher: Bencher, n: usize) {
+    let mut data = black_box(vec![5; n]);
+    let factor = black_box(2);
+
+    bencher.bench_local(|| {
+        for elem in black_box(&mut data) {
+            *elem *= factor;
+        }
+    })
+}
+
+#[divan::bench(
+args = [1_000_000]
+)]
+fn slice_baseline_access(bencher: Bencher, n: usize) {
+    let mut data = black_box(vec![5; n]);
+    let factor = black_box(2);
+
+    bencher.bench_local(|| {
+        for elem in create_iter(black_box(&mut data).into_par_access()) {
+            *elem *= factor;
+        }
+    })
+}
+
+#[divan::bench(
+args = [1_000_000]
+)]
+fn slice_redundantly_indexed_access(bencher: Bencher, n: usize) {
+    let mut data = black_box(vec![5; n]);
+    let factor = black_box(2);
+
+    bencher.bench_local(|| {
+        let indices = 0 .. n;
+        let access = black_box(&mut data).into_par_access();
+        let access = narrow_access_to_indices(access, &indices);
+        for elem in create_iter(access) {
+            *elem *= factor;
+        }
+    })
+}
