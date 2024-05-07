d9568e5b91472a7ea5a131c614114f392bdfd1c0
Andreas Longva <andreas.b.longva@gmail.com>
2024-05-07 15:38:03 +0200

    Move example_with_checked_indices_u32() into rayon_access_slice.rs

--- examples/rayon_access_slice.rs
+++ examples/rayon_access_slice.rs
@@ -5,6 +5,7 @@ use rayon::iter::ParallelIterator;
 fn main() {
     example_with_range();
     example_with_checked_indices();
+    example_with_checked_indices_u32();
 }
 
 fn example_with_range() {
@@ -30,6 +31,27 @@ fn example_with_checked_indices() {
 
     for (idx, elem) in data.into_iter().enumerate() {
         if indices.get_inner().contains(&idx) {
+            assert_eq!(elem, 2.0);
+        } else {
+            assert_eq!(elem, 1.0);
+        }
+    }
+}
+
+fn example_with_checked_indices_u32() {
+    let mut data = vec![1.0; 10000];
+    let index_data: &Vec<u32> = &vec![900, 5, 10, 400, 1000, 100, 200];
+    let indices = index_data
+        .check_unique()
+        .expect("All indices unique")
+        .index_cast();
+
+    let access =
+        narrow_access_to_indices(data.as_mut_slice(), &indices).expect("indices must be unique");
+    create_par_iter(access).for_each(|x| *x *= 2.0);
+
+    for (idx, elem) in data.into_iter().enumerate() {
+        if index_data.contains(&(idx as u32)) {
             assert_eq!(elem, 2.0);
         } else {
             assert_eq!(elem, 1.0);
